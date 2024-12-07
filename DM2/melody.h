94739d898c484d12dcd904a1a9b175aaf50fe1f3
ebdm13 <elie.bellotdesminieres@gmail.com>
2024-12-07 23:56:16 +0100

    until Q15

---
+++ DM2/melody.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "sound.h"
+
+/*Renvoie la fréquence correspondant au pitch*/
+float pitch_to_freq(float pitch);
+
+/*Lis un fichier au format:
+première ligne : le nombre de notes n puis le type de son à utiliser ;
+n lignes suivantes : la hauteur, la durée et le volume de chaque note.
+Et renvoie la piste correspondant.
+*/
+track_t* read_track(FILE* file);
--- DM2/waveform.h
+++ DM2/waveform.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "sound.h"
 
+// Strucure pour les différent types de signaux. Ex: sawtooth, triangle ...
+typedef sound_t* (*signal_t)(float frequence, float amplitude, float duree, int f_ech);
+typedef struct {
+	char* nom;
+	signal_t signale;	
+} signaux_t;
+
+/*renvoi le signal corresondant au nom*/
+signal_t get_signal(char* nom);
+
 /*renvoie les echantillons d'un bruits blanc échantilloné avec une fréquence f_ech prendant une durée duree en secondes*/
 sound_t* white(float duree, int f_ech);
 
