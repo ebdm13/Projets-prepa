8e1278f27797cc8f3765221392522c00c57a89d6
ebdm13 <elie.bellotdesminieres@gmail.com>
2025-04-19 20:47:20 +0200

    dames

--- DM3/rapport.md
+++ DM3/rapport.md
@@ -3,10 +3,20 @@
 **Q1:** Le premier argument est l'appelle du programme: ./satsolver.
 Sans deuxième argument: Fatal error: exception Invalid_argument("index out of bounds")
 
-**Q10::** Soit n le nombre de noeud de l'arbre syntaxique de f. (les noueuds sont les éléments de bases: Not, Or, And, Var, Top, Bot).
+**Q10:** Soit n le nombre de noeud de l'arbre syntaxique de f. (les noueuds sont les éléments de bases: Not, Or, And, Var, Top, Bot).
 Ainsi, on se ramène à un problème de parcours d'arbre binaires avec une étape de traitement (union) en $\Theta(n)$.
 Or on sait que le pire cas pour un parcours en hauteur est le peigne.
 De plus, dans le pire cas toutes les feuilles sont des variables différentes. Et le nombre de feuilles est forcément inférieure aux nombres
 de noeuds et dans le pire cas: peigne, il y a $E(\frac {n+1}2) = \Theta(n)$ feuilles. 
 Pour un tel arbre, l'équation de complexitée s'écrit: $C(n) = C(n-1) + \Theta(n)$.
 Donc $C(n) = \Theta(n)$. Pour optimiser, on peut implémenter var_list avec un ARN ou un AVL.
+
+**Q19:**  Une famille en $\Theta(n^2)$ est un peigne avec que des Or et où les feuilles sont des 
+variables sauf la dernière qui est un Top. i.e "X1 | X2 | ... | Xn | T". Car simpl_step vas parcourir
+tout l'arbre pour faire qu'une seul simplification à chaque fois.
+
+Q25 et 26 à faire
+
+**Q31:** La formule générée par au_plus_une est sous FNC si et seulement si les formules en entrée sont des littéraux. 
+De plus, si on considère que la taille de la formule généré comme la taille de son arbre syntaxique où les feuilles sont
+les formules atomique en entré, alors sa taille est: 3n(n-1) - 1. Avec n le nombre de formules en entrée.
--- DM3/satsolver/satsolver_naif.ml
+++ DM3/satsolver/satsolver_naif.ml
@@ -118,6 +118,72 @@ let from_file (filename: string) : formule =
 	let s = read_lines f in
 	parse s
 
+let test_parse () =
+	assert (parse "a | (b & ~c)" = Or(Var "a", And(Var "b", Not (Var "c"))));
+	assert (parse "a | (b & (c > ~b)) | (a = F)" = Or(Var "a", Or(And(Var "b", implique (Var "c", Not(Var "b"))), equivalence (Var "a", Bot))));
+	assert (parse "((a | b) & c) > d" = implique (And(Or(Var "a", Var "b"), Var "c"), Var "d"));
+	try
+    let _ = parse "a & (b | c" in
+    assert false;
+  	with
+  	| Failure("mauvais parenthésage") -> assert true;
+	
+	(* try
+    let _ = parse "a $ b" in
+    assert false;
+  	with
+  	| Erreur_syntaxe -> assert true; *)
+	print_string "Tests OK\n"
+
+let test_from_file () =
+	assert (from_file "tests/test1.txt" =
+	Or (Not (Or (Var "a", And (Var "b", Not (Var "c")))),
+ And
+  (Or (And (Or (Not (Var "d"), Var "e"), Or (Not (Var "e"), Var "d")),
+    And (Var "f", Not (Var "g"))),
+  Or (Not (Var "h"), Or (Var "i", Var "j")))))
+
+exception Not_enough_arguments of string
+
+(* Renvoie le contenu de la première ligne du fichier file *)
+let read_file (file: string) : string = 
+	let ic = open_in file in
+	let res = input_line ic in
+	close_in ic; res
+
+(* Compte le nombres de OR / AND / NOT dans une formule f *)
+let rec compte_ops (f: formule) : int =
+	match f with
+	| Top | Bot | Var(_) -> 0
+	| Not(f') -> 1 + compte_ops f'
+	| And(f1, f2) | Or(f1, f2) -> 1 + compte_ops f1 + compte_ops f2
+
+let test_compte_ops () = 
+	assert (compte_ops (parse "x | (y & ~z)") = 3);
+	assert (compte_ops (parse "~(x | (x & ~z) | y)") = 5)
+
+(* Vérifie qu'une liste est strictement croissante *)
+let rec is_sort (l: 'a list) : bool =
+	match l with
+	| [] | _::[] -> true
+	| x::y::q -> x < y && is_sort (y::q)
+
+(* Fusionne l1 et l2, deux listes strictement croissante en une nouvelle liste strictement croissante *)
+let rec union (l1: 'a list) (l2: 'a list) : 'a list =
+	match l1, l2 with
+	| l, [] | [], l -> l 
+	| x::q, y::p -> if x < y then x::(union q (y::p)) 
+						else if x > y then y::(union (x::q) p) 
+						else x::(union q p)
+
+(* Retourne la liste des variables de f *)
+let rec var_list (f: formule) : string list =
+	match f with
+	| Top | Bot -> []
+	| Var(q) -> [q]
+	| Not(f') -> var_list f'
+	| And(f1, f2) | Or(f1, f2) -> union (var_list f1) (var_list f2)
+
 let correctionARN (t: 'a noeud_arn) : 'a noeud_arn = 
 	match t with
 	| Noeud(Noir, z, Noeud(Rouge, y, Noeud(Rouge, x, a, b), c), d)
@@ -151,103 +217,75 @@ let var_arn (f: formule) : string arn =
 		| And(f1, f2) | Or(f1, f2) -> insert_var_arn f1 (insert_var_arn f2 t)
 	in insert_var_arn f None
 
-let list_var_from_arn (t: 'a arn) : 'a list =
-	let rec list_arn_aux (t: 'a arn) (l: 'a list) : 'a list = 
+let list_var_from_arn (t: string arn) : string list =
+	let rec list_arn_aux (t: string arn) (l: string list) : string list = 
 		match t with
 		| None -> l
 		| Some Feuille(a) -> a::l
 		| Some Noeud(_, _, t1, t2) -> list_arn_aux (Some t1) (list_arn_aux (Some t2) l)
 	in list_arn_aux t []
 
+(* Evalue f avec la valuation v *)
+let rec evaluate (f: formule) (v: valuation) : bool =
+	match f with
+	| Top -> true
+	| Bot -> false
+	| Var(q) -> List.assoc q v
+	| Not(f') -> not (evaluate f' v)
+	| Or(f1, f2) -> evaluate f1 v || evaluate f2 v
+	| And(f1, f2) -> evaluate f1 v && evaluate f2 v
+
+(* 
+Soit l l'écriture binaire de x. add_one l est l'écriture binaire de x + 1.
+les bits de poids faibles sont à gauche
+*)
+let rec add_one (l: bool list) : bool list =
+	match l with
+	| [] -> [true]
+	| false::q -> true::q
+	| true::q -> false::(add_one q)	
+
+let rec valuation_next (v: valuation) : valuation option =
+	match v with
+	| [] -> Some []
+	| [(_, true)] -> None	
+	| (x, y)::q when y = false -> Some ((x, true)::q)
+	| (x, y)::q -> match valuation_next q with
+	| None -> None
+	| Some q' -> Some ((x, false)::q')
+
 let rec valuation_init (l: string list) : valuation =
 	List.map (fun x -> (x, false)) l
 
-(* renvoie f simplifié si possible et un booléen indiquant si la formule à été simplifiée ou non*)
-let rec simpl_step (f: formule) : formule * bool =
-	match f with
-	| Or(Top, _) | Or(_, Top) | Not Bot -> Top, true
-	| And(Bot, _) | And(_, Bot) | Not Top -> Bot, true
-	| And(Top, f') | And(f', Top) | Or(f', Bot) | Or(Bot, f') | Not(Not f')-> f', true
-	| Or(f1, f2) -> let (f1', x), (f2', y) = simpl_step f1, simpl_step f2 in
-						 Or(f1', f2'), (x || y)
-	| And(f1, f2) -> let (f1', x), (f2', y) = simpl_step f1, simpl_step f2 in
-						 And(f1', f2'), (x || y)
-	| Not f' -> let f2, x = simpl_step f' in Not f2, x
-	| _ -> f, false 
-
-(* Simplifie f si possible *)
-let rec simpl_full (f: formule) : formule =
-	let f', x = simpl_step f in
-	if x then simpl_full f'
-	else f
-
-(* Simplifie f en temps linéaire*)
-let rec simpl (f: formule) : formule =
-  match f with
-  | Top | Bot -> f
-  | Not Top -> Bot
-  | Not Bot -> Top
-  | Not (Not f') -> simpl f'
-  | Not f' -> 
-      let f'' = simpl f' in
-      (match f'' with
-       | Top -> Bot
-       | Bot -> Top
-       | Not fc -> simpl fc
-       | _ -> Not f'')
-
-  | And (f1, f2) ->
-      let f1' = simpl f1 in
-      let f2' = simpl f2 in
-      (match f1', f2' with
-       | Bot, _ | _, Bot -> Bot
-       | Top, f3 | f3, Top -> f3
-       | _ -> And(f1', f2'))
-
-  | Or (f1, f2) ->
-      let f1' = simpl f1 in
-      let f2' = simpl f2 in
-      (match f1', f2' with
-       | Top, _ | _, Top -> Top
-       | Bot, f3 | f3, Bot -> f3
-       | _ -> Or(f1', f2'))
-
-  | _ -> f
-
-(* Remplace v dans f par g *)
-let rec subst (f: formule) (v: string) (g: formule) : formule =
-	match f with
-	| Var x when x = v -> g
-	| Bot | Top | Var _ -> f
-	| Not f' -> Not (subst f' v g)
-	| Or(f1, f2) -> Or(subst f1 v g, subst f2 v g)
-	| And(f1, f2) -> And(subst f1 v g, subst f2 v g)
-
-let quine (f: formule) : sat_result =
-	let rec quine_aux (f: formule) (v: valuation): sat_result = 
-		if f = Top then Some []
-		else if f = Bot then None
-		else match v with
-		| [] -> quine_aux (simpl f) [] 
-		| (x, _)::q -> let f' = simpl (subst f x Top) in
-			match quine_aux f' q with
-			| Some v' -> Some ((x, true)::v')
-			| None -> let f' = simpl (subst f x Bot) in
-				match quine_aux f' q with
-				| None -> None
-				| Some v2' -> Some ((x, false)::v2')
-	in 
+let satsolver_naif (f: formule) : sat_result =
 	let var_l = list_var_from_arn (var_arn f) in
 	let valo = valuation_init var_l in
-	quine_aux f valo
+	let rec satsolver_aux (f: formule) (v: valuation) : sat_result =
+		if evaluate f v then Some v else 
+		match valuation_next v with
+		| None -> None
+		| Some v' -> satsolver_aux f v'
+	in satsolver_aux f valo
+
+let test () = 
+	test_parse ();
+	test_from_file ();
+	test_compte_ops ();
+	print_string "Tous les tests ont réussi\n"
+
+(* let main () =
+	if Array.length Sys.argv <> 2 then
+		raise (Not_enough_arguments "il faut un argument")
+	else
+		match Sys.argv.(1) with
+		| "test" -> test ()
+		| file -> print_string (read_file file) ; print_newline () *)
 
 let rec print_true (v: valuation) : unit = 
 	match v with
 	| [] -> ()
 	| (x, true)::q -> print_string x; print_newline (); print_true q
 	| (x, false)::q -> print_true q	
-
-exception Not_enough_arguments of string
 
 let main () =
 	if Array.length Sys.argv <> 2 then
@@ -257,13 +295,12 @@ let main () =
 		(* | "test" -> test () *)
 		| file ->
 			let t0 = Sys.time () in
-			let res = quine (from_file file) in
+			let res = satsolver_naif (from_file file) in
 			let time = Sys.time () -. t0 in
 			print_string "Temps d'execution: "; print_float time; print_string "s\n";
 			match res with
 			| None -> print_string "La formule n'est pas satisfiable\n"
 			| Some v -> print_string "La formule est satisfiable en assignant 1 aux variables suivantes et 0 aux autres:\n";
 				print_true v
-
 
 let _ = main ()
